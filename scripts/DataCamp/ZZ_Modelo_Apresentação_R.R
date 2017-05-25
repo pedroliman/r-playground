afc1e082974d191d7d3998e0376d73482ab1c571
pedroliman <pedrolima.n@gmail.com>
2017-05-25 15:14:11 -0300

    padrões para usar apresentações no R

---
+++ scripts/DataCamp/ZZ_Modelo_Apresentação_R.R
@@ -0,0 +1,21 @@
+#'Título
+#'========================================================
+#'author: Pedro Nascimento de Lima
+#'date:  
+#'autosize: true
+
+#'Elevando ao Quadrado
+#'========================================================
+#'Vamos Aprender a elevar ao quadrado...
+# Comentário no Código
+a = 2
+b = a^2
+b
+
+#'Fazendo um histograma
+#'========================================================
+#'Um Histograma é fácil de fazer no R!
+# Comentário no Código
+dados = 2:200
+dados = dados*rnorm(1,mean=2,sd=2.3)
+hist(dados)
