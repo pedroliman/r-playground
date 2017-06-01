b999a590d125d19f847af657d291faca5db9d4af
pedroliman <pedrolima.n@gmail.com>
2017-06-01 13:50:04 -0300

    Fatores = Variaveis Categoricas Ordinais e Nominais

--- scripts/DataCamp/Intro to R/03.Factors.R
+++ scripts/DataCamp/Intro to R/03.Factors.R
@@ -14,6 +14,7 @@ gender_vector <- c("Male", "Female", "Female", "Male", "Male")
 factor_gender_vector <- factor(gender_vector)
 factor_gender_vector
 
+
 #' Tipos de Variáveis Categóricas
 #' ========================================================
 #' Existem dois tipos de variáveis categóricas: Nominais e Ordinais. No R, é possível definir isto da segunte forma:
@@ -28,3 +29,71 @@ temperature_vector <- c("High", "Low", "High","Low", "Medium")
 factor_temperature_vector <- factor(temperature_vector, order = TRUE, levels = c("Low", "Medium", "High"))
 factor_temperature_vector
 summary(factor_temperature_vector)
+
+high = factor_temperature_vector[3]
+medium = factor_temperature_vector[2]
+
+#' Níveis de Fatores
+#' ========================================================
+# Extraindo fatores de uma Survey
+survey_vector <- c("M", "F", "F", "M", "M")
+factor_survey_vector <- factor(survey_vector)
+
+# Especificando Níveis de Fatores
+levels(factor_survey_vector) <- c("Female","Male")
+
+# Observação Importante: O R atribui os fatores alfabéticamente quando nenhuma ordem é especificada a priori. Então é importante sempre lembrar de também mudar os nomes deles de modo alfabético.
+factor_survey_vector
+
+#' Usando o Summary de um Fator
+#' ========================================================
+# Aqui vamos Resumir Variáveis. Podemos fazer isso por variáveis ou por colunas
+# Build factor_survey_vector with clean levels
+survey_vector <- c("M", "F", "F", "M", "M")
+factor_survey_vector <- factor(survey_vector)
+levels(factor_survey_vector) <- c("Female", "Male")
+factor_survey_vector
+
+# Generate summary for survey_vector
+summary(survey_vector)
+
+# Generate summary for factor_survey_vector
+summary(factor_survey_vector)
+
+# Male
+male <- factor_survey_vector[1]
+
+# Female
+female <- factor_survey_vector[2]
+
+# Battle of the sexes: Male 'larger' than female?
+male > female
+
+#' Criando Fatores Ordenados e Mostrando o Resultado
+#' ========================================================
+# Criando um Vetor de Velocidades de Carros
+speed_vector <- c("fast", "slow", "slow", "fast", "insane")
+
+# Criando um fator a partir deste vetor
+factor_speed_vector <- factor(speed_vector,
+                              ordered = TRUE,
+                              levels = c("slow", "fast", "insane")
+)
+
+# Mostrando o Fator e Seus Resumos
+factor_speed_vector
+summary(factor_speed_vector)
+
+#' Comparando Fatores Ordinais
+#' ========================================================
+speed_vector <- c("fast", "slow", "slow", "fast", "insane")
+factor_speed_vector <- factor(speed_vector, ordered = TRUE, levels = c("slow", "fast", "insane"))
+
+# Factor value for second data analyst
+da2 <- factor_speed_vector[2]
+
+# Factor value for fifth data analyst
+da5 <- factor_speed_vector[5]
+
+# Is data analyst 2 faster than data analyst 5?
+da2 > da5
